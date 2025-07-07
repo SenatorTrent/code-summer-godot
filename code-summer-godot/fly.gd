51f11d016d9425b22773bb6ef9839937a761566d
artcheong <arthurcheongmvla@gmail.com>
2025-07-07 16:36:53 -0700

    fly movement

--- code-summer-godot/fly.gd
+++ code-summer-godot/fly.gd
@@ -1,24 +1,15 @@
 extends CharacterBody2D
 
-
-
-var target = Vector2(0, 0)
-var grappled = false
-var tongueSpring
 const SPEED = 800.0
 const JUMP_VELOCITY = -800.0
 
+#@onready var timer = $Timer  # Assuming Timer is a child node
+
 func _ready():
-	#tongueSpring = get_node("tongueSpring")
 	pass
-
-func _input(event):
-	# Use is_action_pressed to only accept single taps as input instead of mouse drags.
-	if event is InputEventMouseButton:
-		target = get_global_mouse_position()
-		grappled  = true
-
+	
 func _physics_process(delta: float) -> void:
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	while true:
+		velocity.x = move_toward(velocity.x, randf_range(-3000.0, 3000.0), SPEED)
+		move_and_slide()
+		await get_tree().create_timer(3000.0).timeout
